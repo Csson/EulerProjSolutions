66e7ac8e7a7449207f4efeb74de71d2503d41437
Erik Carlsson <erik.carlsson@live.com>
2013-12-01 21:25:52 +0100

    Builds the grid a little nicer

--- bin/0011-largest-product-in-a-grid.pl
+++ bin/0011-largest-product-in-a-grid.pl
@@ -42,7 +42,7 @@ sub main {
                 $maxes->{'rightup'} = $rightup if $rightup > $maxes->{'rightup'};
             }
             if($rindex < scalar @{ $grid->[$rindex]} - 4 && $cindex < scalar @{ $grid->[$rindex]} - 4) {
-            	my $rightdown = $grid->[ $rindex ][ $cindex ] 
+                my $rightdown = $grid->[ $rindex ][ $cindex ] 
                               * $grid->[ $rindex + 1 ][ $cindex + 1 ] 
                               * $grid->[ $rindex + 2 ][ $cindex + 2 ] 
                               * $grid->[ $rindex + 3 ][ $cindex + 3 ];
@@ -57,7 +57,7 @@ sub main {
 
 
 sub get_grid {
-    my $grid_string = qq{08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
+    my @grid_array = qw{08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
 49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00
 81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65
 52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91
@@ -77,12 +77,11 @@ sub get_grid {
 20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16
 20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54
 01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48};
-    my @rows = split m{\n} => $grid_string;
     my $grid = [];
-    foreach my $row_index (0 .. scalar @rows - 1) {
-        my $row = $rows[ $row_index ];
-        $grid->[ $row_index ] = [ split m{\s} => $row ];
+    while(scalar @grid_array) {
+        push @{ $grid } => [ splice @grid_array, 0, 20 ];
     }
+    
     return $grid;
 }
 
