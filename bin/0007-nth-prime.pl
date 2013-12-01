eb769816c9b56bebf0366da7317201517ab6561f
Erik Carlsson <erik.carlsson@live.com>
2013-12-01 18:16:53 +0100

    Problem 7

---
+++ bin/0007-nth-prime.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use v5.18;
+
+use Time::HiRes qw/time tv_interval/;
+
+my $start = [ time ];
+
+my $nth_prime = $ARGV[0] // 10001;
+main();
+say 'Done in ' . sprintf ('%.5f' => tv_interval $start) . ' seconds.';
+
+sub main {
+    my @primes = (2);
+    
+    my $number = 2;
+    
+    NUMBER:
+    while(++$number) {
+        
+        PRIME:
+        foreach my $prime (@primes) {
+            next NUMBER if $number % $prime == 0;
+        }
+        push @primes => $number;
+        last NUMBER if scalar @primes == $nth_prime;
+    }
+    say "The $nth_prime prime number is $number";
+}
+
+=pod
+
+Problem 7 - 10001st prime
+
+By listing the first six prime numbers: 2, 3, 5, 7, 11 and 13, we can see that the 6th prime is 13.
+
+What is the 10001st prime number?
+
+
+=cut
